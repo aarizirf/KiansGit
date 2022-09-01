40fc7677706bb48849b8ebb47afb13ee162155df
kianchen <bluekian10@gmail.com>
2022-09-01 10:15:41 -0700

    added constructor which takes file content and generates sha1

--- src/blobindex/blob.java
+++ src/blobindex/blob.java
@@ -1,5 +1,24 @@
 package blobindex;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.nio.file.*;
 
 public class blob {
-
+	private String fileContent;
+	public String sha1; 
+	
+	public blob (Path filePath) throws FileNotFoundException, IOException {
+		fileContent = Files.readString(filePath);
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-1");
+	        digest.reset();
+	        digest.update(fileContent.getBytes("utf8"));
+	        sha1 = String.format("%040x", new BigInteger(1, digest.digest()));
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
 }
