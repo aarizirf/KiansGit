66cb1f47c29c129597aef709651bc1017c46a134
kianchen <bluekian10@gmail.com>
2022-09-06 16:11:02 -0700

    added remove method and created write method which writes to index.txt

--- src/blobindex/index.java
+++ src/blobindex/index.java
@@ -13,13 +13,43 @@ public class Index {
 	
 	public void init() {
 		indexFile = new File ("test/index.txt");
-		File objDir = new File ("test/objects");
+		new File ("test/objects").mkdirs();
 	}
 	
 	public void add(String fileName) throws FileNotFoundException, IOException {
 		Blob blobby = new Blob("./test/" + fileName);
 		pairs.put(fileName, blobby.getSha1());
+		write();
+	}
+	
+	public void remove(String fileName) throws FileNotFoundException {
+		File myObj = new File("test/objects/" + pairs.get(fileName) + ".txt"); 
+	    if (myObj.delete()) { 
+	    	System.out.println("Deleted the file: " + myObj.getName());
+	    } 
+	    else {
+	    	System.out.println("Failed to delete the file.");
+	    } 
+		pairs.remove(fileName);
+		indexFile.delete();
+		indexFile = new File ("test/index.txt");
+		write();
+	}
+	
+	private void write() throws FileNotFoundException {
 		PrintWriter printWrite = new PrintWriter (indexFile);
-		printWrite.println(pairs);
+		for (String key: pairs.keySet()) {
+			printWrite.println (key + " : " + pairs.get(key));
+		}
+		printWrite.close();
+	}
+	
+	public static void main (String [] args) throws FileNotFoundException, IOException {
+		Index git = new Index();
+		git.init();
+		git.add("foo.txt");
+		git.add("bar.txt");
+		git.add("foobar.txt");
+		git.remove("foo.txt");
 	}
 }
