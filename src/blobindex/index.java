97874c6e329b2369c24d0e8825f728bbd4082398
kianchen <bluekian10@gmail.com>
2022-09-06 08:52:08 -0700

    started index added init and add methods

--- src/blobindex/index.java
+++ src/blobindex/index.java
@@ -1,5 +1,22 @@
 package blobindex;
 
-public class index {
+import java.util.*;
+import java.io.*;
+
+public class Index {
+	private HashMap<String, String> pairs; 
+	private File indexFile;
 	
+	public Index() {
+		pairs = new HashMap<String, String>();
+	}
+	
+	public void init() {
+		indexFile = new File ("test/index.txt");
+		File objDir = new File ("test/objects");
+	}
+	
+	public void add(String fileName) {
+		
+	}
 }
