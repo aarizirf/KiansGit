a1e8db7fc998a2305261a89527009d290da7c608
kianchen <bluekian10@gmail.com>
2022-09-06 08:50:29 -0700

    Revert "added filecontent getter and index init and started add method"
    This reverts commit eb327a34e7c96b91f940bdb292fc3e01be8b76bd.

--- src/blobindex/index.java
+++ src/blobindex/index.java
@@ -1,18 +1,5 @@
 package blobindex;
 
-import java.util.*;
-import java.io.*;
-
-public class Index {
-	private HashMap<String, String> pairs; 
-	private File indexFile;
+public class index {
 	
-	public Index() {
-		pairs = new HashMap<String, String>();
-	}
-	
-	public void init() {
-		indexFile = new File ("test/index.txt");
-		File objDir = new File ("test/objects");
-	}
 }
